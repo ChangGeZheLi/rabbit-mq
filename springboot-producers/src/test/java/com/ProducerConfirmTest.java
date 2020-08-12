1e2c2a8fe7635c5ba547c65a71d0def2c582ecd2
admin <admin>
2020-08-12 11:42:10 +0800

    完成消费端的ack确认

--- springboot-producers/src/test/java/com/ProducerConfirmTest.java
+++ springboot-producers/src/test/java/com/ProducerConfirmTest.java
@@ -95,7 +95,6 @@ public class ProducerConfirmTest {
             }
         });
 
-
         rabbitTemplate.convertAndSend(RabbitMQConfirm.EXCHANGE_NAME,"confirm","ACK confirmed");
     }
 }
---
+++ springboot_consumers/src/main/java/com/springboot_consumers/listener/ConsumerListener.java
@@ -0,0 +1,54 @@
+package com.springboot_consumers.listener;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @Description: 消费端消息确认监听器
+ */
+@Component
+public class ConsumerListener {
+
+    /**
+     *
+     * @Param message 队列中的消息
+     * channel 当前的消息队列
+     * tag 取出来当前消息在队列中的索引
+     * 用@Header(AmqpHeaders.DELIVERY_TAG)注解拿到
+     **/
+    @RabbitListener(queues = "springboot_queue_confirm")
+    public void ackListener(Message message, Channel channel , @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
+
+        try {
+            //打印消息
+            System.out.println(new String(message.getBody()));
+            //处理业务逻辑
+            System.out.println("处理业务逻辑===================");
+            //手动签收
+            channel.basicAck(tag,true);
+        }catch (Exception e){
+            /**
+             *第三个参数：requeue：重回队列，如果设置为true，则消息重回到queue，broker会重新发送消息
+             **/
+            channel.basicNack(tag,true,true);
+        }
+    }
+
+    /**
+     * Spring 实现 Consumer ACK机制
+     *  设置手动签收：acknowledge=“manual”
+     *  让监听器实现ChannelAwareMessageListener接口
+     *  如果消息成功处理，调用channel的basicAck签收
+     *  如果消息处理失败，则调用channel的basicNack拒绝签收，broker重新发送给consumer
+     * @Param [message, channel]
+     **/
+
+}
